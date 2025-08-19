972025319f5b9f25b714755c1b526996f271fe8b
tohoangnam2 <tohoangnam03@gmail.com>
2025-08-19 09:45:52 +0700

    Initial Commit

---
+++ StoryMakerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StoryMakerApp
+//
+//  Created by to hoang nam on 19/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StoryMakerApp/StoryMakerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryMakerAppApp.swift
+//  StoryMakerApp
+//
+//  Created by to hoang nam on 19/8/25.
+//
+
+import SwiftUI
+
+@main
+struct StoryMakerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
