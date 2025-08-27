5989525dc00e52ca8118554c287c6cfad01fa06b
tohoangnam2 <tohoangnam03@gmail.com>
2025-08-27 17:24:36 +0700

    bug zoom , xoay

---
+++ StoryMakerApp/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  StoryMakerApp
+//
+//  Created by to hoang nam on 27/8/25.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
