3fa0443bbd499416ef9d6c27c94b8ab90ac9aa67
tohoangnam2 <tohoangnam03@gmail.com>
2025-08-29 17:33:29 +0700

    zoom ok , drag rotate vẫn bị giựt

--- StoryMakerApp/Test.swift
+++ StoryMakerApp/Test.swift
@@ -2,44 +2,14 @@
 //  Test.swift
 //  StoryMakerApp
 //
-//  Created by to hoang nam on 27/8/25.
+//  Created by Nam To on 29/8/25.
 //
 
 import SwiftUI
 
 struct Test: View {
-    @State var angle: Angle = .degrees(0)
     var body: some View {
-        Image("img_edit_xoay")
-            .frame(width: 50, height: 50)
-            .padding(20)
-            .contentShape(Rectangle())
-            .rotationEffect(angle)
-            .gesture(
-                LongPressGesture(minimumDuration: 0.2)
-                    .sequenced(before: RotationGesture())
-                    .onChanged { value in
-                        switch value {
-                        case .second(true, let rotation?):
-                            angle = rotation
-                        default:
-                            break
-                        }
-                    }
-                    .onEnded { value in
-                        switch value {
-                        case .second(true, let rotation?):
-                            withAnimation(.easeInOut) {
-                                angle = Angle(
-                                    degrees: angle.degrees.truncatingRemainder(dividingBy: 360)
-                                )
-                            }
-                        default:
-                            break
-                        }
-                    }
-            )
-        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
