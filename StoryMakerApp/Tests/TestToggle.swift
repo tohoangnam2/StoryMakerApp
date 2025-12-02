350e88fa86b902cb18c228ef0922b92f1aa68484
tohoangnam2 <tohoangnam03@gmail.com>
2025-12-02 14:26:50 +0700

    còn export logic mới nữa

---
+++ StoryMakerApp/Tests/TestToggle.swift
@@ -0,0 +1,18 @@
+//
+//  TestToggle.swift
+//  StoryMakerApp
+//
+//  Created by Nam To on 27/11/25.
+//
+
+import SwiftUI
+
+struct TestToggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestToggle()
+}
